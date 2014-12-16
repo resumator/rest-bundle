7a22228ecf453fcabecf92c01a216d148766810c
Justin Beaver <justin.beaver@theresumator.com>
2014-12-16 11:51:12 -0500

    METAREST-2 LimitCriteria unit test

---
+++ Tests/Criteria/LimitCriteriaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lemon\RestBundle\Tests\Criteria;
+
+use Lemon\RestBundle\Criteria\LimitCriteria;
+
+class LimitCriteriaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function asDoctrineTest()
+    {
+        $criteria = new LimitCriteria(999);
+
+        $qb = CriteriaQueryBuilderFactory::build($this);
+
+        $qb->select('p')->from('Lemon\RestBundle\Tests\Fixtures\Person', 'p');
+
+        $criteria->asDoctrine($qb, 'p');
+
+        $this->assertSame(999, $qb->getMaxResults());
+    }
+}
