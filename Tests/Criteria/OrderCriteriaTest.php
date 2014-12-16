3aa6c3b1ec5f3e4ac7d510671900f8a89524fe42
Justin Beaver <justin.beaver@theresumator.com>
2014-12-16 11:58:29 -0500

    METAREST-2 Adds OrderCriteria unit test

---
+++ Tests/Criteria/OrderCriteriaTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lemon\RestBundle\Tests\Criteria;
+
+use Lemon\RestBundle\Criteria\OrderCriteria;
+use Lemon\RestBundle\Criteria\OrderDirection;
+
+class OrderCriteriaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function asDoctrineTest()
+    {
+        $criteria = new OrderCriteria('favoriteColor', new OrderDirection());
+
+        $qb = CriteriaQueryBuilderFactory::build($this);
+
+        $qb->select('p')->from('Lemon\RestBundle\Tests\Fixtures\Person', 'p');
+
+        $criteria->asDoctrine($qb, 'p');
+
+        $this->assertSame('SELECT p FROM Lemon\RestBundle\Tests\Fixtures\Person p ORDER BY p.favoriteColor ASC', $qb->getDQL());
+    }
+}
